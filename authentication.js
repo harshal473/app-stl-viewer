a071d41f77c61964beee7b1aebd1fec7619a4c7f
harshal473 <64724775+harshal473@users.noreply.github.com>
2020-05-03 20:00:02 +0530

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -5,7 +5,7 @@ var OnshapeStrategy = require('passport-onshape').Strategy;
 
 var oauthClientId;
 var oauthClientSecret;
-var callbackUrl = "https://onshape-app-stl.herokuapp.com/oauthRedirect";
+var callbackUrl = "https://newURL-from-heroku.herokuapp.com/oauthRedirect";
 var oauthUrl = 'https://oauth.onshape.com';
 var apiUrl = 'https://cad.onshape.com';
 
